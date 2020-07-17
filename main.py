382c8c76635f20630f07451430fdb42091bfbf1f
NotZen82 <sboldyrev@gmail.com>
2020-07-17 12:35:02 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def trade_spider(max_pages):
+    page = 1
+    while page <= max_pages:
+        url = 'https://monster-book.com/front?page=' + str(page)
+        source_code = requests.get(url)
+        plain_text = source_code.text
+        soup = BeautifulSoup(plain_text)
+        for link in soup.findAll({"a": "title"}):
+            href = "https://monster-book.com" + link.get("href")
+            title = link.string
+            print(title)
+            print(href)
+        page += 1
+
+
+trade_spider(1)
